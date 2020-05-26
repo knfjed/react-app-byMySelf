c04c60593e11ef54dc3b23548abbb8055bd1a8fd
knfjed <knfjed@gmail.com>
2020-05-27 03:47:18 +0900

    JSX dataTable

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let dom = document.querySelector("#root");
-let element = React.createElement("p", {}, "Hello React App!!!");
+let element = React.createElement("p", {}, [
+  React.createElement("h2", {}, "Hello"),
+  React.createElement("h3", {}, "React sample page"),
+  React.createElement("ul", {}, [
+    React.createElement("li", {}, "first item"),
+    React.createElement("li", {}, "second item"),
+    React.createElement("li", {}, "third item"),
+  ]),
+]);
 ReactDOM.render(element, dom);
